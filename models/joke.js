4188594e19f10b46725d17498fb1a5e87d9c6eaf
Corentin <cortech12@gmail.com>
2019-11-29 10:29:57 +0100

    init

---
+++ models/joke.js
@@ -0,0 +1,21 @@
+class Joke {
+    constructor(author, text) {
+        this.date = new Date();
+        this.author = author;
+        this.text = text;
+        this.date = afficheDate()
+    }
+}
+
+function afficheDate() {
+    return this.date.getDate() + "/" + (this.date.getMonth() + 1) + "/" + this.date.getFullYear();
+}
+
+function diplayjokes() {
+    for (var i = 0; i < jokes.length; i++) {
+        for (var joke in jokes) {
+            console.log(jokes[i].getAuthor());
+            //  + 'a dit la blague : ' + jokes.text[joke].author + ' le ' + jokes.date[joke].author + ' .'
+        }
+    }
+}
